a1d5b5134bfc249963c3f5dea583d89df87d2166
Akshayalekshmi <akshayamakairam@gmail.com>
2022-07-08 12:51:27 +0530

    viewgalary

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewgalaryComponent } from './viewgalary/viewgalary.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewgalaryComponent
   ],
   imports: [
     BrowserModule,
